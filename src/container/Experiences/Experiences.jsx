7d2dc9234cffdcfa5195dd4d13f61f4f0aad8d4a
navidabdi <artsanaat@gmail.com>
2022-03-19 18:12:46 +0330

    remove react-tooltip from Experiences

--- src/container/Experiences/Experiences.jsx
+++ src/container/Experiences/Experiences.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
+import { client } from '../../client';
 import './Experiences.scss';
 
 const Experiences = () => {
