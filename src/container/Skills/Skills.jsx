aed7c5e7d32a360c8bd903099a12d4455ac86832
navidabdi <artsanaat@gmail.com>
2022-03-19 18:09:35 +0330

    remove react-tooltip

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
